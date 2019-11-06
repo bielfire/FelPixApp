d2bad2e88c13a074328d14d7bc632e295b775ecf
Gabriel moreira <bielfire@gmail.com>
2019-11-06 16:43:54 -0300

    Adicionado animações de botões da cena loja

--- FelPixApp/Cenas/CenaLoja.swift
+++ FelPixApp/Cenas/CenaLoja.swift
@@ -41,18 +41,37 @@ class CenaLoja: SKScene {
         botaoLojaTitulo = SKSpriteNode(imageNamed: "botaoLoja")
         botaoLojaTitulo.texture?.filteringMode = .nearest
         botaoLojaTitulo.setScale(2.3)
-        botaoLojaTitulo.position = CGPoint(x: self.size.width/2, y: self.size.height - botaoLojaTitulo.size.height/2 - 10)
+        //botaoLojaTitulo.position = CGPoint(x: self.size.width/2, y: self.size.height - botaoLojaTitulo.size.height/2 - 10)
+        botaoLojaTitulo.position.x = self.size.width / 2
+        botaoLojaTitulo.position.y = self.size.height * -2
         botaoLojaTitulo.zPosition = 1
+        botaoLojaTitulo.alpha = 0.2
+        
+        let animacaoEntra = SKAction.moveTo(y: self.size.height - botaoLojaTitulo.size.height/2 - 10, duration: 0.75)
+        let animacaoAlpha = SKAction.fadeAlpha(to: 1, duration: 0.25)
+        let animationSequence = SKAction.sequence([animacaoEntra, animacaoAlpha ])
+        
+        botaoLojaTitulo.run(animationSequence)
         
         self.addChild(botaoLojaTitulo)
+        
     }
     
     func setupBotaoLojaSemente() {
         botaoLojaSemente = SKSpriteNode(imageNamed: "botaoLojaSemente")
         botaoLojaSemente.texture?.filteringMode = .nearest
         botaoLojaSemente.setScale(0.85)
-        botaoLojaSemente.position = CGPoint(x: self.size.width/2, y: botaoLojaTitulo.position.y - botaoLojaSemente.size.height/2 - 30)
+        //botaoLojaSemente.position = CGPoint(x: self.size.width/2, y: botaoLojaTitulo.position.y - botaoLojaSemente.size.height/2 - 30)
+        botaoLojaSemente.position.x = self.size.width / 2
+        botaoLojaSemente.position.y = self.size.height * -2
         botaoLojaSemente.zPosition = 2
+        botaoLojaSemente.alpha = 0.2
+        
+        let animacaoEntra = SKAction.moveTo(y: self.size.height - botaoLojaSemente.size.height/2 - 58, duration: 0.75)
+        let animacaoAlpha = SKAction.fadeAlpha(to: 1, duration: 0.25)
+        let animationSequence = SKAction.sequence([SKAction.wait(forDuration: 0.5), animacaoEntra, animacaoAlpha ])
+        
+        botaoLojaSemente.run(animationSequence)
         
         self.addChild(botaoLojaSemente)
     }
@@ -61,8 +80,17 @@ class CenaLoja: SKScene {
         botaoLojaEstrela = SKSpriteNode(imageNamed: "botaoLojaEstrela")
         botaoLojaEstrela.texture?.filteringMode = .nearest
         botaoLojaEstrela.setScale(0.85)
-        botaoLojaEstrela.position = CGPoint(x: self.size.width/2, y: botaoLojaSemente.position.y - botaoLojaEstrela.size.height - 10)
+        //botaoLojaEstrela.position = CGPoint(x: self.size.width/2, y: botaoLojaSemente.position.y - botaoLojaEstrela.size.height - 10)
+        botaoLojaEstrela.position.x = self.size.width / 2
+        botaoLojaEstrela.position.y = self.size.height * -2
         botaoLojaEstrela.zPosition = 2
+        botaoLojaEstrela.alpha = 0.2
+        
+        let animacaoEntra = SKAction.moveTo(y: self.size.height - botaoLojaEstrela.size.height/2 - 173, duration: 0.75)
+        let animacaoAlpha = SKAction.fadeAlpha(to: 1, duration: 0.25)
+        let animationSequence = SKAction.sequence([SKAction.wait(forDuration: 1.0), animacaoEntra, animacaoAlpha ])
+        
+        botaoLojaEstrela.run(animationSequence)
         
         self.addChild(botaoLojaEstrela)
     }
@@ -71,8 +99,17 @@ class CenaLoja: SKScene {
         botaoLojaSombra = SKSpriteNode(imageNamed: "botaoLojaSombra")
         botaoLojaSombra.texture?.filteringMode = .nearest
         botaoLojaSombra.setScale(0.85)
-        botaoLojaSombra.position = CGPoint(x: self.size.width/2, y: botaoLojaEstrela.position.y - botaoLojaEstrela.size.height/2 - botaoLojaSombra.size.height/2 - 10)
+        // botaoLojaSombra.position = CGPoint(x: self.size.width/2, y: botaoLojaEstrela.position.y - botaoLojaEstrela.size.height/2 - botaoLojaSombra.size.height/2 - 10)
+        botaoLojaSombra.position.x = self.size.width / 2
+        botaoLojaSombra.position.y = self.size.height * -2
         botaoLojaSombra.zPosition = 2
+        botaoLojaSombra.alpha = 0.2
+        
+        let animacaoEntra = SKAction.moveTo(y: self.size.height - botaoLojaSombra.size.height/2 - 292, duration: 0.75)
+        let animacaoAlpha = SKAction.fadeAlpha(to: 1, duration: 0.25)
+        let animationSequence = SKAction.sequence([SKAction.wait(forDuration: 1.5), animacaoEntra, animacaoAlpha ])
+        
+        botaoLojaSombra.run(animationSequence)
         
         self.addChild(botaoLojaSombra)
     }
@@ -81,8 +118,17 @@ class CenaLoja: SKScene {
         botaoLojaRestaura = SKSpriteNode(imageNamed: "botaoLojaRestaura")
         botaoLojaRestaura.texture?.filteringMode = .nearest
         botaoLojaRestaura.setScale(0.85)
-        botaoLojaRestaura.position = CGPoint(x: self.size.width/2, y: botaoLojaSombra.position.y - botaoLojaSombra.size.height/2 - botaoLojaRestaura.size.height/2 - 10)
+        //botaoLojaRestaura.position = CGPoint(x: self.size.width/2, y: botaoLojaSombra.position.y - botaoLojaSombra.size.height/2 - botaoLojaRestaura.size.height/2 - 10)
+        botaoLojaRestaura.position.x = self.size.width / 2
+        botaoLojaRestaura.position.y = self.size.height * -2
         botaoLojaRestaura.zPosition = 2
+        botaoLojaRestaura.alpha = 0.2
+        
+        let animacaoEntra = SKAction.moveTo(y: self.size.height - botaoLojaRestaura.size.height/2 - 380, duration: 0.75)
+        let animacaoAlpha = SKAction.fadeAlpha(to: 1, duration: 0.25)
+        let animationSequence = SKAction.sequence([SKAction.wait(forDuration: 2.0), animacaoEntra, animacaoAlpha ])
+        
+        botaoLojaRestaura.run(animationSequence)
         
         self.addChild(botaoLojaRestaura)
     }
@@ -91,10 +137,20 @@ class CenaLoja: SKScene {
         botaoSair = SKSpriteNode(imageNamed: "botaoSair")
         botaoSair.texture?.filteringMode = .nearest
         botaoSair.setScale(2.3)
-        botaoSair.position = CGPoint(x: self.size.width/2, y: botaoLojaRestaura.position.y - botaoLojaRestaura.size.height/2 - botaoSair.size.height/2 - 10)
+        //botaoSair.position = CGPoint(x: self.size.width/2, y: botaoLojaRestaura.position.y - botaoLojaRestaura.size.height/2 - botaoSair.size.height/2 - 10)
+        botaoSair.position.x = self.size.width / 2
+        botaoSair.position.y = self.size.height * -2
         botaoSair.zPosition = 2
+        botaoSair.alpha = 0.2
+        
+        let animacaoEntra = SKAction.moveTo(y: self.size.height - botaoSair.size.height/2 - 418, duration: 0.75)
+        let animacaoAlpha = SKAction.fadeAlpha(to: 1, duration: 0.25)
+        let animationSequence = SKAction.sequence([SKAction.wait(forDuration: 2.5), animacaoEntra, animacaoAlpha ])
+        
+        botaoSair.run(animationSequence)
         
         self.addChild(botaoSair)
+        
     }
     
     func setupImagemFundo() {
@@ -103,7 +159,9 @@ class CenaLoja: SKScene {
         imagemFundo.zPosition = 0
         imagemFundo.size.width = self.size.width
         imagemFundo.size.height = self.size.height
-        imagemFundo.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        //imagemFundo.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        imagemFundo.position.x = self.size.width/2
+        imagemFundo.position.y = self.size.height/2
         
         self.addChild(imagemFundo)
     }
